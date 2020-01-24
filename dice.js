f93e8f817624eff015bc2783e6efdf98f3616485
vex-xcc <vex.xcc@gmail.com>
2020-01-24 10:31:11 +0300

    I corrected my answer in QA 1 and 2

--- dice.js
+++ dice.js
@@ -8,23 +8,33 @@ Example: rollDice() // Dice rolled are 6 and 1. Sum is 7
 BONUS: have your function take an argument of the # of dice to be rolled.*/
 
 const rollDice = function(){
-  const ar1 = [1,2,3,4,5,6];
-  const random = Math.floor(Math.random() * 6);
-  const random2 = Math.floor(Math.random() * 6);
-  return    num = ar1[random]+ar1[random2]
 
-  }
+  const myarray = [1,2,3,4,5,6];
+
+   const random = Math.floor(Math.random() * 6);
+   const random2 = Math.floor(Math.random() * 6);
+
+  const num = myarray[random]+myarray[random2]
+
+  return   console.log('Dice rolled are  '+myarray[random] +
+   ' and ' + myarray[random2]+' = '+num)
+}
 
 
   /*
   BONUS: have your function take an argument of the # of dice to be rolled.
    Example: rollDice(3) // Dice rolled are 4, 2, and 7. Sum is 13.
 */
-const rollDice = function(argument){
-  const ar1 = [1,2,3,4,5,6];
-  const random = Math.floor(Math.random() * 6);
-  const random2 = Math.floor(Math.random() * 6);   
-   const num = ar1[random]+ar1[random2]
+const rollDiceBonus = function(argument){
+  const myarray = [1,2,3,4,5,6];
 
-  return    userAr = argument + num
+    const random = Math.floor(Math.random() * 6);
+    const random2 = Math.floor(Math.random() * 6);   
+
+   
+   const sum = myarray[random]+myarray[random2];
+   const num = sum + argument;
+
+   return   console.log('Dice rolled are  '+myarray[random] +
+    ' , ' + myarray[random2]+' and '+ argument +' = '+num)
   }
--- rps.js
+++ rps.js
@@ -1,32 +1,29 @@
+const userChoice = function(userChoice){
 
-//you have to run this function first and pick one of 'paper', 'rock', 'scissors'
-let userChoice =function(ChoiceOne){
-    return ChoiceOne
-}
 
-//after that you can run all code
-let choiceStack = ['paper', 'rock', 'scissors'];
-
-let random = Math.floor(Math.random() * 3);
-
-let computerChoice = choiceStack[random];
-
-console.log(`Your choice is ${userChoice}, the computer's choice is ${computerChoice}.`);
-
-if (userChoice === computerChoice){
-console.log("Tie!");
-}else if (userChoice === 'paper' && computerChoice === 'rock'){
-console.log("You win!");
-}else if (userChoice === 'rock' && computerChoice === 'scissors'){
-console.log("You win!");
-}else if (userChoice === 'scissors' && computerChoice === 'rock'){
-console.log("You lose!");
-}else if (userChoice === 'rock' && computerChoice === 'paper'){
-console.log("You lose!");
-}else if (userChoice === 'paper' && computerChoice === 'scissors'){
-console.log("You lose!");
-}else if (userChoice === 'scissors' || computerChoice === 'paper'){
-console.log("You win!");
-}else{
-console.log("Invalid input, please try again");
-}
+    const choiceStack = ['paper', 'rock', 'scissors'];
+    
+    const random = Math.floor(Math.random() * 3);
+    
+    const computerChoice = choiceStack[random];
+    
+     
+    if (userChoice === computerChoice){
+    console.log('Your choice is ' + userChoice + ' the computers choice is ' + computerChoice + ' Tie! ');
+    }else if (userChoice === 'paper' && computerChoice === 'rock'){
+    console.log(' Your choice is ' + userChoice + ' the computers choice is ' + computerChoice+' You win! ');
+    }else if (userChoice === 'rock' && computerChoice === 'scissors'){
+    console.log('Your choice is ' + userChoice + ' the computers choice is ' + computerChoice+' You win! ');
+    }else if (userChoice === 'scissors' && computerChoice === 'rock'){
+    console.log('Your choice is ' + userChoice + ' the computers choice is ' + computerChoice+' You lose! ' );
+    }else if (userChoice === 'rock' && computerChoice === 'paper'){
+    console.log('Your choice is ' + userChoice + ' the computers choice is ' + computerChoice+' You lose! ');
+    }else if (userChoice === 'paper' && computerChoice === 'scissors'){
+    console.log('Your choice is ' + userChoice + ' the computers choice is ' + computerChoice+' You lose! ');
+    }else if (userChoice === 'scissors' || computerChoice === 'paper'){
+    console.log('Your choice is ' + userChoice + ' the computers choice is ' + computerChoice+' You win! ');
+    }else{
+    console.log("Invalid input, please try again");    
+    
+    }
+    }
